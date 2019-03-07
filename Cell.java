5822db6af5a394baee09b14d92b4b8e3cf6cc572
amybubu <48263133+amybubu@users.noreply.github.com>
2019-03-07 14:52:28 -0800

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,4 +1,15 @@
+package Mancala;
+
+import java.util.ArrayList;
 
 public class Cell {
-
+	int x;
+	int y;
+	ArrayList<Pebble> cell;
+	
+	public Cell(){
+		int x = 100;
+		int y = 100;
+		ArrayList<Pebble> cell = new ArrayList<Pebble>();
+	}
 }
