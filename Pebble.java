381691ea7cc84d96be332f4860810773c122ae88
amybubu <48263133+amybubu@users.noreply.github.com>
2019-03-08 14:27:42 -0800

    Update Pebble.java

--- Pebble.java
+++ Pebble.java
@@ -1,4 +1,37 @@
+package Mancala;
+
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Pebble {
+	int x;
+	int y;
+	Color c;
+	
+	public Pebble(int x, int y, Color c){
+		this.x = x;
+		this.y = y;
+		this.c = c;
+	}
 
+	public void paint(Graphics g) {
+		g.fillOval(0, 0, 800, 800);
+		g.setColor(c);
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
 }
