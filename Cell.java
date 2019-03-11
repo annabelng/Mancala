0eaaf10c92d5f91b0ab06ab4afd103bd6079fca9
amybubu <48263133+amybubu@users.noreply.github.com>
2019-03-11 14:16:28 -0700

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,16 +1,28 @@
-import java.util.List;
+package Mancala;
 
-
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class Cell {
 	int x;
 	int y;
+	Color c;
 	ArrayList<Pebble> cell;
 	
-	public Cell(){
-		this.x = 100;
-		this.y = 100;
+	public Cell(int x, int y, Color c){
+		this.x = x;
+		this.y = y;
+		this.c = c;
 		cell = new ArrayList<Pebble>();
+		
+		Pebble p1 = new Pebble(x,y);
+		Pebble p2 = new Pebble(x,y+20);
+		Pebble p3 = new Pebble(x+20,y);
+		Pebble p4 = new Pebble(x+20,y+20);
+		
+		cell.add(0,p1);
+		cell.add(1,p2);
+		cell.add(2,p3);
+		cell.add(3,p4);
 	}
 }
