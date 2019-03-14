047176124e2df266e9e8fa515fdb234f0892bd67
annabelng <45742119+annabelng@users.noreply.github.com>
2019-03-14 15:06:57 -0700

    Add files via upload

--- Pebble.java
+++ Pebble.java
@@ -1,6 +1,11 @@
 package Mancala;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.awt.image.ImageObserver;
 import java.io.File;
 
 import javax.swing.ImageIcon;
@@ -9,37 +14,33 @@ import javax.swing.JLabel;
 public class Pebble {
 	private int x;
 	private int y;
-	private int w, h;
 	
-	private JLabel img;
-	
+	Image img;
+	AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
+
 	public Pebble(String filename){
 		
-		String src =  new File("").getAbsolutePath()+"/src/";
-		ImageIcon pebble = new ImageIcon(src+filename);
-		//connect the file to the img
-		img = new JLabel(pebble);
-		
-		x = 100;
-		y = 200;
-		w = 50;
-		h = 52;
-		
-		img.setBounds(x,y,w,h);
-		
-	}
-	public JLabel getImg(){
-		img.setBounds(x,y,w,h);
-		return img; //getter for object's img
-	}
+		String src = new File("").getAbsolutePath() + "/src/";
+		ImageIcon ast = new ImageIcon(src + filename);
+		x = 0;
+		y = 0;
 
-	public void paint(Graphics g) {
-		
-		g.fillOval(x, y, 50, 50);
-		
+		img = getImage(filename);
+	
 	}
 	
-	public int getX(){
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+
+		g2.drawImage(img, x, y, this);
+	}
+
+	
+	public void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(2, 2);
+	}
+	/*public int getX(){
 		return x;
 	}
 	
@@ -53,5 +54,10 @@ public class Pebble {
 	
 	public void setY(int y){
 		this.y = y;
+	}*/
+	private Image getImage(String path) {
+
+		img = Toolkit.getDefaultToolkit().getImage("pebble.png");
+		return img;
 	}
 }
