75140915f9ee3b5570afe46680eb464b3a3ffcfe
amybubu <48263133+amybubu@users.noreply.github.com>
2019-03-15 13:54:22 -0700

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,9 +1,13 @@
 package Mancala;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.util.ArrayList;
+import java.applet.Applet;
 
-public class Cell {
+import javax.swing.JPanel;
+
+public class Cell extends JPanel{
 	int x;
 	int y;
 	Color c;
@@ -14,15 +18,54 @@ public class Cell {
 		this.y = y;
 		this.c = c;
 		cell = new ArrayList<Pebble>();
-		
-		Pebble p1 = new Pebble(x,y);
-		Pebble p2 = new Pebble(x,y+20);
-		Pebble p3 = new Pebble(x+20,y);
-		Pebble p4 = new Pebble(x+20,y+20);
-		
-		cell.add(0,p1);
-		cell.add(1,p2);
-		cell.add(2,p3);
-		cell.add(3,p4);
+
 	}
+
+	public void paint(Graphics g, int x, int y, int size){
+		super.paintComponents(g);
+		
+		g.fillOval(x, y, size, size);
+		
+		Pebble p = new Pebble(100,200,c);
+		//p1.paint(g);
+		//p1.paint(g);
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	ArrayList<ArrayList<Integer>> cells = new ArrayList<ArrayList<Integer>>();
+	
+	public void cellClicked (int n){
+		if (x == n) {
+			if (cells.get(x).size() >= cells.size() - x) {
+				for (int i = x + 1; i < cells.size(); i++) {
+					ArrayList<Integer> cell = cells.get(i);
+					cell.add(0, 0);
+					cells.get(x).remove(0);
+				}
+				int temp = cells.get(x).size();
+				for (int i = 0; i < temp; i++) {
+					ArrayList<Integer> cell = cells.get(i);
+					cell.add(0, 0);
+					cells.get(x).remove(0);
+				}
+			} else {
+				int temp = cells.get(x).size();
+				for (int i = x + 1; i <= x + temp; i++) {
+					ArrayList<Integer> cell = cells.get(i);
+					cell.add(0, 0);
+					cells.get(x).remove(0);
+				}
+			}
+			System.out.println(cells);
+		}
+	}
+	
+	
 }
